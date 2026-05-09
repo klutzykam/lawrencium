a9e0d182e83092351b53f308cc4b897bdd888ff1
klutzykam <thisistaufei@gmail.com>
2026-05-09 11:51:16 +0500

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,28 +2,15 @@
 
 set -ouex pipefail
 
-### Install packages
-
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
-
-# this installs a package from fedora repos
 dnf5 install -y tmux
 dnf5 install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release -y
 dnf5 install -y mangowc
 dnf5 -y copr enable avengemedia/dms-git
 dnf5 -y copr enable avengemedia/danklinux
 
-dnf5 -y install niri dms dms-greeter
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
+dnf5 -y install dms dms-greeter foot
 
-#### Example for enabling a System Unit File
+dms setup greeter -y
 
 systemctl enable podman.socket
+systemctl enable --user dms.service
