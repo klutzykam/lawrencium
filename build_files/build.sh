98e0fd2d4028db5263ffa0f56b441eed9fc36106
klutzykam <thisistaufei@gmail.com>
2026-05-09 12:39:15 +0500

    s

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,11 +4,11 @@ set -ouex pipefail
 
 dnf5 install -y tmux
 dnf5 install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release -y
-dnf5 install -y mangowc
+dnf5 install -y mangowc gnome-keyring
 dnf5 -y copr enable avengemedia/dms-git
 dnf5 -y copr enable avengemedia/danklinux
 
-dnf5 -y install dms dms-greeter foot quickshell-git23
+dnf5 -y install dms dms-greeter foot quickshell-git 
 
 systemctl enable podman.socket
 systemctl enable greetd.service
