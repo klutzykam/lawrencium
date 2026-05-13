3b508f67a328bb8bc1f57bf6755efc41f46dfb22
klutzykam <thisistaufei@gmail.com>
2026-05-14 00:31:55 +0500

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,8 +2,8 @@
 
 set -ouex pipefail
 pacman-key --init
-pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com --noconfirm
-pacman-key --lsign-key 3056513887B78AEB --noconfirm
+pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com 
+pacman-key --lsign-key 3056513887B78AEB
 pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' --noconfirm
 pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm
 echo "\n[chaotic-aur]\n Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf 
