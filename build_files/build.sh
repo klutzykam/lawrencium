4a8f24c25f3f914d33f2b7f3652b062b8ad6b540
klutzykam <thisistaufei@gmail.com>
2026-05-14 00:29:51 +0500

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,10 +2,10 @@
 
 set -ouex pipefail
 pacman-key --init
-#pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
-#pacman-key --lsign-key 3056513887B78AEB
-#pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
-#pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
-#echo "\n[chaotic-aur]\n Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
+pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com --noconfirm
+pacman-key --lsign-key 3056513887B78AEB --noconfirm
+pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' --noconfirm
+pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm
+echo "\n[chaotic-aur]\n Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf 
 
-pacman -Syu base bash bash-completion curl gcc git nano tar unzip linux nvidia-open nvidia-utils efibootmgr linux-firmware plasma plasma-login-manager konsole dolphin pipewire wireplumber udev ostree --no-confirm
+pacman -Syu base bash bash-completion curl gcc git nano tar unzip linux nvidia-open nvidia-utils efibootmgr linux-firmware plasma plasma-login-manager konsole dolphin pipewire wireplumber udev ostree --noconfirm
