cff2f7bef0f702ab2ee1ec20b234839c609960ab
klutzykam <thisistaufei@gmail.com>
2026-05-16 19:27:29 +0500

    chuink it again . Chud

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,25 +2,26 @@
 
 set -ouex pipefail
 pacman-key --init
-pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com 
+pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 pacman-key --lsign-key 3056513887B78AEB
 pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' --noconfirm >> /dev/null
 pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm >> /dev/null
-echo -e "\n[chaotic-aur]\n Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf 
+echo -e "\n[chaotic-aur]\n Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
 
 #pacman -R linux --noconfirm >> /dev/null
 pacman -Syu nvidia-open nvidia-utils --noconfirm >> /dev/null
-pacman -Syu curl gcc sudo nano noto-fonts noto-fonts-cjk noto-fonts-emoji tar unzip efibootmgr plasma plasma-login-manager konsole dolphin pipewire wireplumber chaotic-aur/opentabletdriver flatpak lvm2 tpm2-tss dhcpcd fastfetch --noconfirm
+pacman -Syu curl gcc sudo nano noto-fonts noto-fonts-cjk noto-fonts-emoji tar unzip efibootmgr plasma plasma-login-manager ark power-profiles-daemon konsole dolphin pipewire wireplumber chaotic-aur/opentabletdriver flatpak lvm2 tpm2-tss dhcpcd fastfetch --noconfirm
+pacman -Rns discover --noconfirm >> /dev/null
 pacman -Syu git podman podman-compose distrobox udiskie udisks2 firewalld networkmanager libmtp --noconfirm >> /dev/null
 
-#copied from tartaria will replace / understand later...
+# rechunker fix
+chmod +x /usr/bin/rechunker-group-fix
+systemctl enable rechunker-group-fix
+
+# dracut.... copied from tartaria, will replace later...
 KERNEL_VERSION="$(basename "$(find /usr/lib/modules -maxdepth 1 -type d | grep -v -E "\.img$" | tail -n 1)")"
 DRACUT_NO_XATTR=1 dracut --force --no-hostonly --reproducible --zstd --verbose --add ostree "/usr/lib/modules/$KERNEL_VERSION/initramfs.img"
 
 echo "Enable Systemd services."
-systemctl enable \
-    plasmalogin.service \
-    NetworkManager.service \
-    firewalld.service   \
-    systemd-resolved.service 
-systemctl enable --global opentabletdriver.service
+systemctl preset-all
+systemctl preset-all --global
